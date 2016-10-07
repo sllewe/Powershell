4c06abd67daa059d7a204720e76cd3280343fdb2
SeanL <sllewe@gmail.com>
2016-10-07 00:06:06 -0400

    Add ReleaseVersioning.ps1

---
+++ ReleaseVersioning.ps1
@@ -0,0 +1,10 @@
+﻿#Take in $buildnumber and set as html page at $deployedserverlocation
+
+Param([Parameter(Mandatory=$true)][string]$buildnumber, [string]$deployserver, [string]$environmentmessage, [string]$deployedserverlocation)
+
+$statuspage  = @"
+<body> <h1> $environmentmessage </h1> <p> Build Number $buildnumber </p> <br> <p> Deployed from $deployserver </p> </body>  </body>
+"@
+ 
+ 
+ $statuspage | ConvertTo-Html -body $statuspage | out-file $deployedserverlocation
